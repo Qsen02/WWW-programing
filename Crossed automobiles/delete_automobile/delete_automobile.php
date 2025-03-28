10ab1ac10b19761eda9123d56f2992e8559c5652
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-03-28 23:42:01 +0200

    Delete automobile updated

--- Crossed automobiles/delete_automobile/delete_automobile.php
+++ Crossed automobiles/delete_automobile/delete_automobile.php
@@ -2,8 +2,13 @@
     include "../constants.php";
     if (isset($_GET["id"])) {
         $automobile_id = $_GET["id"];
-        $deleteQuery = "DELETE FROM automobile WHERE id={$automobile_id};";
-        if (!mysqli_query($dbConnection, $deleteQuery)) {
+        $driver_id= $_GET["driver_id"];
+        $deleteAutomobileQuery = "DELETE FROM automobile WHERE id={$automobile_id};";
+        if (!mysqli_query($dbConnection, $deleteAutomobileQuery)) {
+            echo "<h2 align='center'>Грешка при изтриването на данните!</h2>";
+        }
+        $deleteDriverQuery = "DELETE FROM drivers WHERE id={$driver_id};";
+        if (!mysqli_query($dbConnection, $deleteDriverQuery)) {
             echo "<h2 align='center'>Грешка при изтриването на данните!</h2>";
         }
     }else{
