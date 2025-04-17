01de08e68f28a3c922940feab8f4fbfb10e43e35
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-04-17 22:48:22 +0300

    Implemented register form

--- Crossed automobiles/edit_automobile/edit_automobile.php
+++ Crossed automobiles/edit_automobile/edit_automobile.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Преминали автомобили</title>
     <link rel="stylesheet" href="../add_automobile/add_automobile.css">
-    <script src="../validation.js"></script>
+    <script src="../automobile_validation.js"></script>
 </head>
 
 <body>
